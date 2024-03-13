f1d2ee6b7560b0492cfff5ef4497f4f784dd7ec8
tttuuu13 <56789432+tttuuu13@users.noreply.github.com>
2024-03-14 02:06:55 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,17 +7,14 @@ public class Program
     //string token = System.Environment.GetEnvironmentVariable("TOKEN");
     string token = "7123360905:AAE-89fU1NoXeX9JVhIADHRsoBQpNn1h7wY";
     Bot bot = new Bot(token);
-    while (true)
+    try
     {
-      try
-      {
-        await bot.Run();
-        Console.ReadLine();
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e);
-      }
+      await bot.Run();
+      Console.ReadLine();
+    }
+    catch (Exception e)
+    {
+      Console.WriteLine(e);
     }
   }
 }
