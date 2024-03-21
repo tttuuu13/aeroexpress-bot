2d2d3c65dc17589b5714980c3c673abbae156b5f
tttuuu13 <tim.ib.222005@gmail.com>
2024-03-21 23:31:44 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ public class Program
   {
     // Not storing sensetive data in the code, using Heroku config variables instead.
     string token = System.Environment.GetEnvironmentVariable("TOKEN");
-    Bot bot = new Bot(token, "../../../logs.txt");
+    Bot bot = new Bot(token, "../../../tmp/logs.txt");
     await bot.Run();
   }
 }
